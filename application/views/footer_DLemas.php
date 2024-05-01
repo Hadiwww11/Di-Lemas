7fbfdf29471303a36d30ee9aa64dd5537a1c8fa2
hadifauziwiratmaza <hadisadut11@gmail.com>
2024-05-01 20:52:17 +0700

    awal commit

---
+++ application/views/footer_DLemas.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="footer-container">
+        <div class="social-media">
+            <h4>Temukan Kami Di Sosial Media</h4>
+            <ul class="social-icons">
+                <li><a href="https://www.facebook.com/delimas" target="_blank" class="social-icon"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/delimas" target="_blank" class="social-icon"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="https://www.twitter.com/delimas" target="_blank" class="social-icon"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="mailto:delimas@yahoo.com" class="social-icon"><i class="fas fa-envelope"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
